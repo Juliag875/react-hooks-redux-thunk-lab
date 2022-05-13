af3790cd913f7c5db3b2e5b427a8c373d6d2cf67
Iuliia El Sammak <juliag875@gmail.com>
2022-05-12 22:38:03 -0400

    commit tree 8ff9aea8ec189947bffb0ae3cb3d70acbcfd19e2

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,15 @@
 // write your CatList component here
+import React from 'react'
+
+function CatList({catPics}) {
+  return (
+    <div>
+      {catPics.map((pic) => (
+        <img key={pic.id} src={pic.url} alt="cat" height="200"/>
+      )) 
+      }
+    </div>
+  )
+}
+
+export default CatList
--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,38 @@
-export function fetchCats() {}
+// import { bindActionCreators } from "redux";
 
-export default function catsReducer() {}
+ export function fetchCats() {
+   return function (dispatch) {
+    dispatch({type: "cats/catsLoading"});
+    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+      .then(res => res.json())
+      .then(data => {
+        dispatch({ type: "cats/catsLoaded", payload: data.images });
+      })
+   }
+}
+
+const initialState = {
+    entities: [],    //arr of cats
+    status: "idle",  //loading state
+  }
+
+export default function catsReducer( state=initialState, action ) {
+  switch (action.type) {
+    case "cats/catsLoading":
+      return {
+        ...state,
+        status: "loading",
+      };
+      case "cats/catsLoaded":
+        return {
+          ...state,
+          entities: action.payload,
+          status: "idle"
+        };
+        default: 
+        return state;
+    }
+  }
+
+// 1. The word “async” before a function means one simple thing: a function always returns a promise. 
+// 2. The keyword "await" makes JavaScript wait until that promise settles and returns its result.
